39e66d03d9d33d683f309d5f22842040ce9733d3
Callum Suttie <callumsuttie1@gmail.com>
2025-12-05 09:37:04 +1300

    Fix bug where logger prints two entries

--- src/clinflow/logging_utils.py
+++ src/clinflow/logging_utils.py
@@ -10,26 +10,26 @@ def get_logger(name):
     # create logger
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        # create console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
 
-    # create console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+        # create file handler
+        fh = logging.FileHandler("logs/clinflow.log")
+        fh.setLevel(logging.DEBUG)
 
-    # create file handler
-    fh = logging.FileHandler("logs/clinflow.log")
-    fh.setLevel(logging.DEBUG)
+        # create formatter
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
 
-    # create formatter
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+        # add formatter both handlers
+        ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
 
-    # add formatter both handlers
-    ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
-
-    # add both handlers to logger
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+        # add both handlers to logger
+        logger.addHandler(ch)
+        logger.addHandler(fh)
 
     return logger
